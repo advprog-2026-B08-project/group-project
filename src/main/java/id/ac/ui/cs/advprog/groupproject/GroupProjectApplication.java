6f995d6bf4fd21cc3406605a9c4da72ced477a82
Andrew Wanarahardja <andrew.wanarahardja@gmail.com>
2026-02-19 23:03:49 +0700

    init project

---
+++ src/main/java/id/ac/ui/cs/advprog/groupproject/GroupProjectApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.groupproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupProjectApplication.class, args);
+    }
+
+}
