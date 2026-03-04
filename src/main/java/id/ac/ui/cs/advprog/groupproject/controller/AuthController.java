088659ec8b7f9d54599ecc54b17b712a46adc4f4
IkanBerkepalaLele <ehgojim@gmail.com>
2026-03-04 19:46:55 +0700

    feat: added catalog mvp

--- src/main/java/id/ac/ui/cs/advprog/groupproject/controller/AuthController.java
+++ src/main/java/id/ac/ui/cs/advprog/groupproject/controller/AuthController.java
@@ -36,6 +36,8 @@ public class AuthController {
         User user = new User();
         user.setUsername(username);
         user.setPassword(passwordEncoder.encode(password));
+        //TODO: Nanti tolong diganti ke opsi user, jangan default jastiper
+        user.setRole("Jastiper");
 
         userRepository.save(user);
 
--- src/main/java/id/ac/ui/cs/advprog/groupproject/model/Catalog.java
+++ src/main/java/id/ac/ui/cs/advprog/groupproject/model/Catalog.java
@@ -1,6 +1,9 @@
 package id.ac.ui.cs.advprog.groupproject.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.hibernate.annotations.UuidGenerator;
 import java.util.UUID;
@@ -15,14 +18,24 @@ public class Catalog {
     @UuidGenerator
     private UUID id;
 
+    @NotBlank(message = "Product name is required")
     private String name;
+    
     private String description;
+    
+    @NotNull(message = "Price is required")
+    @Min(value = 0, message = "Price cannot be negative")
     private Double price;
+    
+    @NotNull(message = "Stock is required")
+    @Min(value = 0, message = "Stock cannot be negative")
     private Integer stock;
     
+    @NotNull(message = "originLocation is required")
     @Column(name = "origin_location")
     private String originLocation;
 
+    @NotNull(message = "travelDate is required")
     @Column(name = "travel_date")
     private LocalDate travelDate;
 
