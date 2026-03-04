65fdf31a6e05e5544b9e847f705e352d569a2423
IkanBerkepalaLele <ehgojim@gmail.com>
2026-03-04 12:38:44 +0700

    build: changed spring v4 to v3.5.1

--- src/main/java/id/ac/ui/cs/advprog/groupproject/GroupProjectApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/groupproject/GroupProjectApplication.java
@@ -2,7 +2,7 @@ package id.ac.ui.cs.advprog.groupproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import io.github.cdimascio.dotenv.Dotenv;
 
