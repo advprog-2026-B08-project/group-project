bf82a3a1e42a3a705a9248c05d4b9bdaedbd4913
Clairine <clairine.christabel@ui.ac.id>
2026-04-16 19:28:04 +0700

    feat(order/test): add Top Up 10 Juta shortcut button in Order List to ease checkout testing

--- src/main/java/id/ac/ui/cs/advprog/groupproject/order/controller/OrderWebController.java
+++ src/main/java/id/ac/ui/cs/advprog/groupproject/order/controller/OrderWebController.java
@@ -1,11 +1,14 @@
 package id.ac.ui.cs.advprog.groupproject.order.controller;
 
 import id.ac.ui.cs.advprog.groupproject.order.service.OrderService;
+import id.ac.ui.cs.advprog.groupproject.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/order")
@@ -13,10 +16,17 @@ public class OrderWebController {
 
     @Autowired
     private OrderService orderService;
+    
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping("/list")
-    public String getAllOrders(Model model) {
+    public String getAllOrders(Model model, Principal principal) {
         model.addAttribute("orders", orderService.findAll());
+        if (principal != null) {
+            userRepository.findByUsername(principal.getName())
+                .ifPresent(user -> model.addAttribute("currentUserId", user.getId()));
+        }
         return "order/list";
     }
 }
