b78f09edd69ac379b0839421beae3309a3554ed7
Clairine <clairine.christabel@ui.ac.id>
2026-04-16 21:22:49 +0700

    restore: DotenvConfig.java and PasswordConfig.java deleted by Revert catalog commit

---
+++ src/main/java/id/ac/ui/cs/advprog/groupproject/config/PasswordConfig.java
@@ -0,0 +1,15 @@
+package id.ac.ui.cs.advprog.groupproject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
